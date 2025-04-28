e11a790b7e935bbe8418586532e62048535bd5ff
SanaKhan-1613026 <s.pattan@eagles.oc.edu>
2025-04-27 23:31:21 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,17 +1,35 @@
+import java.io.File;
+
 public class Account {
 
 	private String accountNumber;
 	private String customerName;
 	private String deliveryFrequency;
+	private double balance;
 
+	// Constructor for testing
+	public Account(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	// Existing methods
 	public File getStatements() {
-		// TODO - implement Account.getStatements
 		throw new UnsupportedOperationException();
 	}
 
 	public File getTaxDocuments() {
-		// TODO - implement Account.getTaxDocuments
 		throw new UnsupportedOperationException();
 	}
-
 }
