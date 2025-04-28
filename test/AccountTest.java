1ead8ed73ee9992cf315c917b07148eb39c8109d
SanaKhan-1613026 <s.pattan@eagles.oc.edu>
2025-04-27 22:48:11 -0500

    Update AccountTest.java

--- test/AccountTest.java
+++ test/AccountTest.java
@@ -4,8 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AccountTest {
 
     @Test
-    public void testAccountNumber() {
-        Account account = new Account("12345");
-        assertEquals("12345", account.getAccountNumber());
+    public void testAccountCreation() {
+        Account account = new Account("ACC12345");
+        assertEquals("ACC12345", account.getAccountNumber());
+    }
+
+    @Test
+    public void testSetAndGetBalance() {
+        Account account = new Account("ACC12345");
+        account.setBalance(500.0);
+        assertEquals(500.0, account.getBalance());
     }
 }
