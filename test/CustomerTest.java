61a4818f672fb1970efb2e535406b1c2bc66258b
SanaKhan-1613026 <s.pattan@eagles.oc.edu>
2025-04-27 22:47:39 -0500

    Update CustomerTest.java

--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -4,6 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CustomerTest {
 
     @Test
+    public void testCustomerName() {
+        Customer customer = new Customer("John Doe");
+        assertEquals("John Doe", customer.getName());
+    }
+
+    @Test
     public void testSetAndGetEmail() {
         Customer customer = new Customer("John Doe");
         customer.setEmail("john@example.com");
